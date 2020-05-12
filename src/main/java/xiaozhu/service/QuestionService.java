d3b66c3bee887babc57a60953c1206a4acaa8968
xiaozhu1 <1778815369@qq.com>
2020-05-12 22:35:29 +0800

    1

--- src/main/java/xiaozhu/service/QuestionService.java
+++ src/main/java/xiaozhu/service/QuestionService.java
@@ -7,7 +7,6 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import ch.qos.logback.core.joran.util.beans.BeanUtil;
 import xiaozhu.dto.QuestionDto;
 import xiaozhu.mapper.QuestionMapper;
 import xiaozhu.mapper.UserMapper;
